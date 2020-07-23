35c12497d367d4e08a1136ea50ac060fc8fa1e29
pavani126 <68629035+pavani126@users.noreply.github.com>
2020-07-23 13:26:48 +0530

    Add files via upload

---
+++ task.py
@@ -0,0 +1,21 @@
+num=int(input('enter your number: '))
+i=2
+if num > 1:
+ if (num % i) == 0:
+   print(num, "is not a prime number")
+ else:
+   print(num, "is a prime number")
+else:
+   print(num, "is not a prime number")
+   
+   
+   
+   
+   
+   
+  output: 
+   
+   enter your number: 4
+(4, 'is not a prime number')
+
+Process finished.
