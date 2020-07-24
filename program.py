0f7b1edc05db9ca015997993689f0c007d08a491
pavani126 <68629035+pavani126@users.noreply.github.com>
2020-07-24 14:32:02 +0530

    Add files via upload

---
+++ program.py
@@ -0,0 +1,16 @@
+num=int(input("enter any number:",))
+total=0
+value=1
+while(value<=num):
+  total=total+value
+  value=value+1
+print("The sum of natural Numbers from 1 to {0}={1}".format(num,total))
+  
+  
+output:
+  
+  
+  enter any number:14
+The sum of natural Numbers from 1 to 14=105
+
+Process finished.
